ac348fd68460357a5cf5d81130c37b4328060661
gitcyco <gitcyco@users.noreply.github.com>
2025-12-01 16:07:51 -0800

    day 1 part 2

--- aoc2025_javascript/README.md
+++ aoc2025_javascript/README.md
@@ -11,7 +11,7 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -69,9 +69,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 3.038ms
-Time part 2: -
-Both parts: 3.038ms
+Time part 1: 1.447ms
+Time part 2: 109.652ms
+Both parts: 111.099ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 3.038ms
+Total stars: 2/50
+Total time: 111.099ms
 ```
 
 <!--/RESULTS-->
--- aoc2025_javascript/src/day01/index.js
+++ aoc2025_javascript/src/day01/index.js
@@ -21,8 +21,42 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
+  let cur = 50;
+  let len = 100;
+  let count = 0;
 
-  return;
+  // Ugly brute force, just simulate it to get a baseline correct answer
+  // const lines = input
+  //   .replace(/[LR]/g, (e) => (e === "L" ? "-" : "+"))
+  //   .split("\n")
+  //   .map(Number)
+  //   .forEach((num) => {
+  //     let abs = Math.abs(num);
+  //     for (let i = 0; i < abs; i++) {
+  //       if (num < 0) cur--;
+  //       else cur++;
+  //       cur = ((cur % len) + len) % len;
+  //       if (cur === 0) count++;
+  //     }
+  //   });
+
+  // Mathing it out properly
+  const lines = input
+    .replace(/[LR]/g, (e) => (e === "L" ? "-" : "+"))
+    .split("\n")
+    .map(Number)
+    .forEach((num) => {
+      let next = num + cur;
+      count += Math.abs(Math.floor(next / len));
+      if (next <= 0) {
+        if (next % len === 0) count++;
+        else if (cur === 0) count--;
+      }
+      cur = ((next % len) + len) % len;
+    });
+
+  console.log("ANSWER:", count);
+  return count.toString();
 };
 
 run({
@@ -47,13 +81,23 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+                L68
+                L30
+                R48
+                L5
+                R60
+                L55
+                L1
+                L99
+                R14
+                L82`,
+        expected: "6",
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
