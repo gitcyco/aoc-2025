23f1f228f5ac5ffc42e1efabe99b46ee67ad462e
gitcyco <gitcyco@users.noreply.github.com>
2025-12-02 23:07:41 -0800

    day 3 part 1 and part 2

--- aoc2025_javascript/README.md
+++ aoc2025_javascript/README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 2.71ms
 
 ```
 Day 02
-Time part 1: 152.406ms
-Time part 2: 609.446ms
-Both parts: 761.851ms
+Time part 1: 138.936ms
+Time part 2: 408.334ms
+Both parts: 547.27ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 8.332ms
+Time part 2: 2.789ms
+Both parts: 11.121ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 764.561ms
+Total stars: 6/50
+Total time: 561.101ms
 ```
 
 <!--/RESULTS-->
--- aoc2025_javascript/src/day03/index.js
+++ aoc2025_javascript/src/day03/index.js
@@ -4,32 +4,85 @@ const parseInput = (rawInput) => rawInput;
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
+  const banks = input.split("\n").map((e) => e.trim());
+  // Get lowest index of each
+  const lowest = [];
+  for (let bank of banks) {
+    const obj = {};
+    for (let i = 0; i < bank.length; i++) {
+      if (bank[i] in obj) continue;
+      obj[bank[i]] = i;
+    }
+    lowest.push(obj);
+  }
+  // Get highest index of each
+  const highest = [];
+  for (let bank of banks) {
+    const obj = {};
+    for (let i = 0; i < bank.length; i++) {
+      obj[bank[i]] = i;
+    }
+    highest.push(obj);
+  }
+  let jolts = [];
+  for (let i = 0; i < banks.length; i++) {
+    let max = 0;
+    for (let num = 11; num < 100; num++) {
+      let [l, h] = num.toString().split("");
+      if (lowest[i][l] < highest[i][h]) max = Math.max(max, num);
+    }
+    jolts.push(max);
+  }
 
-  return;
+  return jolts.reduce((a, e) => a + e, 0).toString();
 };
 
+// Monotonic stacks... forgot about those, oh joy! lol
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
+  const banks = input.split("\n").map((e) => e.trim());
+  let len = 12;
+  let total = 0;
+  for (let bank of banks) {
+    const stack = [];
+    let left = bank.length - len;
+    for (let num of bank) {
+      while (stack.length && +num > stack[stack.length - 1] && left > 0) {
+        stack.pop();
+        left--;
+      }
+      stack.push(num);
+    }
+    total += +stack.slice(0, len).join("");
+  }
 
-  return;
+  return total.toString();
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+                987654321111111
+                811111111111119
+                234234234234278
+                818181911112111`,
+        expected: "357",
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+                987654321111111
+                811111111111119
+                234234234234278
+                818181911112111`,
+        expected: "3121910778619",
+      },
     ],
     solution: part2,
   },
